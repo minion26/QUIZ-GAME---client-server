61e3e8a34b481e2957779e2db29ff6fb5bfd088d
Jitca Diana <65017893+minion26@users.noreply.github.com>
2022-12-04 15:30:15 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -11,56 +11,80 @@
 
 extern int errno;
 
+int quiz_count;
 int port;
 
 int main(int argc, char *argv[])
 {
-    int fdclient; // descriptorul de socket
-    struct sockaddr_in server;
+  int fdclient; // descriptorul de socket
+  struct sockaddr_in server;
 
-    if (argc != 3)
-    {
-        printf("Sintaxa: %s <adresa_server> <port>\n", argv[0]);
-        return -1;
-    }
+  if (argc != 3)
+  {
+    printf("Sintaxa: %s <adresa_server> <port>\n", argv[0]);
+    return -1;
+  }
 
-    /* stabilim portul */
-    port = atoi(argv[2]);
+  /* stabilim portul */
+  port = atoi(argv[2]);
 
-    // creez socket
-    fdclient = socket(AF_INET, SOCK_STREAM, 0);
-    if (fdclient == -1)
-    {
-        perror("[server]Eroare la socket().\n");
-        return errno;
-    }
+  // creez socket
+  fdclient = socket(AF_INET, SOCK_STREAM, 0);
+  if (fdclient == -1)
+  {
+    perror("[server]Eroare la socket().\n");
+    return errno;
+  }
 
-    /* umplem structura folosita pentru realizarea conexiunii cu serverul */
+  /* umplem structura folosita pentru realizarea conexiunii cu serverul */
   /* familia socket-ului */
   server.sin_family = AF_INET;
   /* adresa IP a serverului */
   server.sin_addr.s_addr = inet_addr(argv[1]);
   /* portul de conectare */
-  server.sin_port = htons (port);
+  server.sin_port = htons(port);
 
-  //ma conectez la server
-  int myconnect = connect(fdclient, (struct sockaddr *) &server,sizeof (struct sockaddr));
-  if (myconnect == -1){
-    perror ("[client]Eroare la connect().\n");
+  // ma conectez la server
+  int myconnect = connect(fdclient, (struct sockaddr *)&server, sizeof(struct sockaddr));
+  if (myconnect == -1)
+  {
+    perror("[client]Eroare la connect().\n");
     return errno;
   }
-  char msg[100];
-  bzero (msg, 100);
-  printf ("[client]Introduceti un nume: ");
-  fflush (stdout);
-  read (0, msg, 100);
-  
-  /* trimiterea mesajului la server */
-  if (write (fdclient, msg, 100) <= 0)
-    {
-      perror ("[client]Eroare la write() spre server.\n");
-      return errno;
-    }
 
+  int questionNumber = 0;
+
+  char msg[100];
+  bzero(msg, 100);
+  printf("[client]Introduceti un nume: ");
+  fflush(stdout);
+  read(0, msg, 100);
+
+  /* trimiterea mesajului la server */
+  if (write(fdclient, msg, 100) <= 0)
+  {
+    perror("[client]Eroare la write() spre server.\n");
+    return errno;
+  }
+
+  while (1)
+  {
+    //while cate inrebari am atatea read() si write() am
+
+
+
+
+    // char msg[100];
+    // bzero(msg, 100);
+    // printf("[client] Scrieti ceva: ");
+    // fflush(stdout);
+    // read(0, msg2, 100);
+
+    // if (write(fdclient, msg, 100) <= 0)
+    // {
+    //   perror("[client]Eroare la write() spre server.\n");
+    //   return errno;
+    // }
+  }
   close(fdclient);
 }
--- server.c
+++ server.c
@@ -15,39 +15,268 @@
 #include <string.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <sqlite3.h>
 
 #define PORT 2604
 extern int errno;
 
-typedef struct
+sqlite3 *db; // pointer pentru baza de date
+
+typedef struct thread
 {
     int fdClient; // socketul pt conectarea cu clientul
-    int idThread;
-    bool exit;
+    int idThread; // al catelea thread este
+    bool exited;
     struct sockaddr address;
     struct sockaddr_in client; // adresa clientului conectat
-    char username[100];
+    char username[256];
     int addr_len; // lungimea lui adress
 
 } clientThread;
 
-struct
+struct client
+{
+    char username[256];
+    int points;
+    int socketDescriptor;
+    bool exited;
+
+} clients[6];
+
+struct game
 {
     int numberPlayers;
+    int noQuestion;
 } game;
+
+void play(void *thr)
+{
+    clientThread *info = (clientThread *)thr;
+    int socket = info->fdClient;
+
+    // folosesc select
+    fd_set readfds;
+    fd_set actfds;
+    struct timeval tv;
+    int fd;
+    int nfds = info->fdClient;
+
+    char correctAnswer;
+    char clientCommand[100], clientAnswer[100];
+
+    for (int question = 1; question <= game.noQuestion; question++)
+    {
+        // trimit intrebarea printr o functie
+        // primesc raspunsul
+        correctAnswer = "lalala";
+
+        bzero(clientCommand, 100);
+        FD_ZERO(&actfds);
+        FD_SET(info->fdClient, &readfds);
+
+        tv.tv_sec = 15; // clientul are la dispozitie 15s sa raspunda la intrebare
+        tv.tv_usec = 0;
+
+        int myselect = select(nfds + 1, &readfds, NULL, NULL, &tv);
+        if (myselect < 0)
+        {
+            printf("[thread - %d] eroare la select().\n", info->idThread);
+        }
+
+        // verificam daca clientul a raspuns
+        if (FD_ISSET(info->fdClient, &readfds))
+        {
+            char buffer[100];
+            int bytes;
+
+            bytes = read(info->fdClient, clientAnswer, sizeof(buffer));
+            if (bytes < 0)
+            {
+               printf("[thread -%d] Eroare la read() de la client.\n", info->idThread);
+            }
+
+            //trebuie sa verific daca clientul imi da raspunsul bun
+        }
+    }
+}
 
 void *threadFunction(void *thr)
 {
     // struct clientThread *info;
     // info = ((struct clientThread*)thr);
     clientThread *info = (clientThread *)thr;
-    printf("[thread-%d] - Asteptam mesajul...\n", info->idThread);
+
+    if (info->idThread >= 2)
+    {
+        printf("[server] there are enaugh clients");
+        fflush(stdout);
+        return 0;
+        pthread_detach(pthread_self());
+    }
+
+    printf("[thread-%d] - Introduceti un username:...\n", info->idThread);
     fflush(stdout);
-    //pthread_detach(pthread_self());	
+
+    struct sockaddr_in client = info->client;
+    int socket = info->fdClient;
+    char username[256];
+    bzero(username, 256);
+
+    int myread = read(socket, username, 256);
+    if (myread <= 0)
+    {
+        printf("[thread -%d] - eroare la read().", info->idThread);
+        perror("[server] eroare la read() de la client");
+    }
+
+    strcpy(info->username, username);
+    strcpy(clients[info->idThread].username, username);
+    clients[info->idThread].points = 0; // cate pct are
+    clients[info->idThread].exited = 0; // daca a iesit din joc
+
+    printf("Client %d with username: %s and thread id: %ld is connected\n", info->idThread, info->username, pthread_self());
+    fflush(stdout);
+
+    play(thr);
+
+    // pthread_detach(pthread_self());
+}
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+{
+    int i;
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+        fflush(stdout);
+    }
+    return 0;
+}
+
+void selectAnswer()
+{
+    //iau din baza de date si raspunsul corect, si punctajul
+}
+
+void sendQuestion(struct thread info, int question)
+{
+    int rc = sqlite3_open("test.db", &db);
+
+    if (rc)
+    {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    }
+    else
+    {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    //casting int la char pierde din informatii si nu pot folosit (char)
+    //folosesc sprintf
+    char inregistrare[100];
+    sprintf(inregistrare, "SELECT QUESTION FROM QUIZ WHERE ID=%d", question);
+
+    sqlite3_stmt *stmt;
+    
+    rc = sqlite3_prepare_v2(db, inregistrare, -1, &stmt, NULL);
+
+    if(rc < 0)
+    {
+        printf("Error executing sql statement\n");
+        fflush(stdout);
+    }
+
+    rc = sqlite3_step(stmt);
+    //de continuat
 
 }
+
+void populateTable()
+{
+    char *zErrMsg = 0;
+    char *sql2;
+    int rc = sqlite3_open("test.db", &db);
+
+    if (rc)
+    {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    }
+    else
+    {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    sql2 = "INSERT INTO QUIZ (ID,QUESTION,ANSWER,POINTS) "
+           "VALUES (1, 'How many lives is a cat said to have?', '9', 10); "
+           "INSERT INTO QUESTIONS (ID,QUESTION,ANSWER,POINTS) "
+           "VALUES (2, 'What is the currency of Italy?', 'Euro', 10); "
+           "INSERT INTO QUESTIONS (ID,QUESTION,ANSWER,POINTS) "
+           "VALUES (3, 'Which element is said to keep bones strong?', 'Calcium', 10);";
+
+    rc = sqlite3_exec(db, sql2, callback, 0, &zErrMsg);
+
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        fflush(stdout);
+        sqlite3_free(zErrMsg);
+    }
+    else
+    {
+        fprintf(stdout, "Records created successfully\n");
+        fflush(stdout);
+    }
+
+    sqlite3_close(db);
+}
+
+void createDataBase()
+{
+    // sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc = sqlite3_open("test.db", &db);
+
+    if (rc)
+    {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        fflush(stdout);
+    }
+    else
+    {
+        fprintf(stderr, "Opened database successfully\n");
+        fflush(stdout);
+    }
+
+    char *sql;
+    sql = "CREATE TABLE QUIZ("
+          "ID         INT              NOT NULL, "
+          "QUESTION   VARCHAR(1000)    NOT NULL, "
+          "ANSWER     VARCHAR(1000)    NOT NULL, "
+          "POINTS     INT              NOT NULL);";
+
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        fflush(stdout);
+        sqlite3_free(zErrMsg);
+    }
+    else
+    {
+        fprintf(stdout, "Table created successfully\n");
+        fflush(stdout);
+    }
+
+    populateTable();
+    sqlite3_close(db);
+    
+}
+
 int main()
 {
+
+    createDataBase();
     struct sockaddr_in server;
 
     int fdserver;
@@ -91,6 +320,7 @@ int main()
 
     int id = 0;
     // pentru fiecare client fac thread
+
     while (1)
     {
 
@@ -99,7 +329,6 @@ int main()
 
         printf("[server]Asteptam la portul %d...\n", PORT);
         fflush(stdout);
-
 
         thread_arg = (clientThread *)malloc(sizeof(clientThread));
         thread_arg->idThread = id;
@@ -112,14 +341,17 @@ int main()
             continue;
         }
 
+        clients[thread_arg->idThread].socketDescriptor = thread_arg->fdClient;
+        game.numberPlayers = 0;
+        game.numberPlayers++;
+        game.noQuestion = 3;
+
         tid = pthread_create(&tid, NULL, &threadFunction, thread_arg);
         if (tid < 0)
         {
             perror("[server] eroare la pthread_create()");
-            //free(thread_arg);
+            // free(thread_arg);
             return errno;
         }
-
-       
     }
 }
